9d17ab29b82f56b156e410816cc6920319a2e9ec
Ricaldone-Gabriel <gabriel.ricaldone@jcmaxwell.it>
2024-05-03 09:05:21 +0200

    init

--- server/server.js
+++ server/server.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+const bodyParser = require("body-parser");
+const http = require("http");
+const ejs = require("ejs");
 const port = 3000;
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use("/", express.static(__dirname + "/views"));
+app.set("views", path.join(__dirname, "views"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
