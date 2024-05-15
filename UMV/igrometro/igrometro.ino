ad7eee25bc681cccce78e57770afa85aa2f4ab8a
Ricaldone-Gabriel <gabriel.ricaldone@jcmaxwell.it>
2024-05-15 20:36:00 +0200

    barometro e igrometro

---
+++ UMV/igrometro/igrometro.ino
@@ -0,0 +1,23 @@
+#include "DHT.h"
+#define DHT11PIN 17
+//Wemos D1 MINI ESP32 Bluepad32
+DHT dht(DHT11PIN, DHT11);
+void setup()
+{
+  
+  Serial.begin(115200);
+/* Start the DHT11 Sensor */
+  dht.begin();
+}
+
+void loop()
+{
+  float humi = dht.readHumidity();
+  float temp = dht.readTemperature() + 20;
+  Serial.print("Temperature: ");
+  Serial.print(temp);
+  Serial.print("ºC ");
+  Serial.print("Humidity: ");
+  Serial.println(humi);
+  delay(1000);
+}
