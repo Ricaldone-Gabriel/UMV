4a87538e4a0102b7453c4b1be291133159ded9b3
Ricaldone-Gabriel <gabriel.ricaldone@jcmaxwell.it>
2024-05-27 10:44:01 +0200

    stile e footer

--- server/index.php
+++ server/index.php
@@ -41,79 +41,81 @@
 
 
     <script>
-        window.onload = function() {
-            var chart1 = new CanvasJS.Chart("chartContainer1", {
-                zoomEnabled: true,
-                animationEnabled: true,
-                exportEnabled: false,
-                theme: "dark1", // "light1", "light2", "dark1", "dark2"
-                title: {
-                    text: "Valori ambientali"
-                },
-                axisY: {
-                    title: "Temperatura",
-                    titleFontColor: "#C0504E",
-                    lineColor: "#C0504E",
-                    labelFontColor: "#C0504E",
-                    tickColor: "#C0504E",
-                },
+window.onload = function() {
+    var chart1 = new CanvasJS.Chart("chartContainer1", {
+        zoomEnabled: true,
+        animationEnabled: true,
+        exportEnabled: false,
+        theme: "dark1", // "light1", "light2", "dark1", "dark2"
+        title: {
+            text: "Valori ambientali acquisiti"
+        },
+        axisY: {
+            title: "Temperatura",
+            titleFontColor: "#C0504E",
+            lineColor: "#C0504E",
+            labelFontColor: "#C0504E",
+            tickColor: "#C0504E",
+        },
 
-                axisY2: {
-                    title: "Umidità",
-                    titleFontColor: "#4F81BC",
-                    lineColor: "#4F81BC",
-                    labelFontColor: "#4F81BC",
-                    tickColor: "#4F81BC",
-                },
-                data: [{
-                        type: "line", //change type to bar, line, area, pie, etc  
-                        name: "Temperatura",
-                        xValueType: "dateTime",
-                        dataPoints: <?php echo json_encode($dataPoints1, JSON_NUMERIC_CHECK); ?>
-                    },
-                    {
-                        type: "line", //change type to bar, line, area, pie, etc  
-                        name: "Umidità",
-                        axisYType: "secondary",
-                        xValueType: "dateTime",
-                        dataPoints: <?php echo json_encode($dataPoints2, JSON_NUMERIC_CHECK); ?>
-                    }
-                ]
+        axisY2: {
+            title: "Umidità",
+            titleFontColor: "#4F81BC",
+            lineColor: "#4F81BC",
+            labelFontColor: "#4F81BC",
+            tickColor: "#4F81BC",
+        },
+        data: [{
+                type: "line", //change type to bar, line, area, pie, etc  
+                name: "Temperatura",
+                xValueType: "dateTime",
+                dataPoints: <?php echo json_encode($dataPoints1, JSON_NUMERIC_CHECK); ?>
+            },
+            {
+                type: "line", //change type to bar, line, area, pie, etc  
+                name: "Umidità",
+                axisYType: "secondary",
+                xValueType: "dateTime",
+                dataPoints: <?php echo json_encode($dataPoints2, JSON_NUMERIC_CHECK); ?>
+            }
+        ]
 
-            });
-            var chart2 = new CanvasJS.Chart("chartContainer2", {
-                zoomEnabled: true,
-                animationEnabled: true,
-                exportEnabled: false,
-                theme: "dark1", // "light1", "light2", "dark1", "dark2"
-                axisY: {
-                    title: "Pressione",
-                    titleFontColor: "#4F81BC",
-                    lineColor: "#4F81BC",
-                    labelFontColor: "#4F81BC",
-                    tickColor: "#4F81BC",
-                },
-                data: [{
-                    type: "line", //change type to bar, line, area, pie, etc  
-                    name: "Pressione",
-                    xValueType: "dateTime",
-                    dataPoints: <?php echo json_encode($dataPoints3, JSON_NUMERIC_CHECK); ?>
-                }]
+    });
+    var chart2 = new CanvasJS.Chart("chartContainer2", {
+        zoomEnabled: true,
+        animationEnabled: true,
+        exportEnabled: false,
+        theme: "dark1", // "light1", "light2", "dark1", "dark2"
+        axisY: {
+            title: "Pressione",
+            titleFontColor: "#4F81BC",
+            lineColor: "#4F81BC",
+            labelFontColor: "#4F81BC",
+            tickColor: "#4F81BC",
+        },
+        data: [{
+            type: "line", //change type to bar, line, area, pie, etc  
+            name: "Pressione",
+            xValueType: "dateTime",
+            dataPoints: <?php echo json_encode($dataPoints3, JSON_NUMERIC_CHECK); ?>
+        }]
 
-            });
-            chart1.render();
-            chart2.render();
-        }
+    });
+    chart1.render();
+    chart2.render();
+}
     </script>
 
-    <body>
+    <body class="d-flex flex-column min-vh-100">
         <?php include "./views/partials/navbar.php" ?>
+
         <div class="container-fluid">
 
+            <div id="chartContainer1" style="height: 370px; width: 100%;"></div>
+            <div id="chartContainer2" style="height: 370px; width: 100%;"></div>
         </div>
-        <div id="chartContainer1" style="height: 370px; width: 100%;"></div>
-        <div id="chartContainer2" style="height: 370px; width: 100%;"></div>
         <script src="https://cdn.canvasjs.com/canvasjs.min.js"></script>
+        <?php include "./views/partials/footer.php" ?>
     </body>
 
     </html>
---
+++ server/views/partials/footer.php
@@ -0,0 +1,13 @@
+<footer class="container-fluid mt-auto">
+    <div class="row pt-3 justify-content-md-center">
+        <div class="col mx-3 col-lg-3">
+            <p>Email: gabriel.ricaldone@jcmaxwell.it</p>
+            <a href="https://github.com/Ricaldone-Gabriel/UMV">Github</a>
+        </div>
+        <div class="col mx-3 col-lg-3">
+            <p>&copy; 2024 Gabriel Ricaldone</p>
+            <p>Realizzato da Gabriel Ricaldone</p>
+        </div>
+    </div>
+</footer>
+</body>
